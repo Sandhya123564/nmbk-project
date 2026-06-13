479064eb3e568ed512bfe9a5eea385295f7150f1
sandhya <sandhyadsan03@gmail.com>
2026-06-13 20:37:59 +0530

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,61 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/contact", (req, res) => {
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+app.post("/contact", async (req, res) => {
   const { name, email, message } = req.body;
 
-  console.log("Name:", name);
-  console.log("Email:", email);
-  console.log("Message:", message);
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: "New Contact Form Message - NMBK",
+    text: `
+Name: ${name}
 
-  res.json({ success: true, message: "Message sent successfully" });
+Email: ${email}
+
+Message:
+${message}
+    `,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.json({
+      success: true,
+      message: "Message sent successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Email sending failed",
+    });
+  }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
 app.get("/", (req, res) => {
   res.send("NMBK Backend Running Successfully");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(Server running on port ${PORT});
 });

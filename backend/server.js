39b0a7071dcd81dbaba13ce739087be4537c0acb
sandhya <sandhyadsan03@gmail.com>
2026-07-08 20:03:27 +0530

    Use Gmail SMTP with environment variables

--- backend/server.js
+++ backend/server.js
@@ -3,40 +3,34 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const nodemailer=require("nodemailer")
+const { Resend } = require("resend");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 app.post("/contact", async (req, res) => {
   const { name, email, message } = req.body;
 
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_USER,
-    subject: "New Contact Form Message - NMBK",
-    text: `
+
+  try {
+    const info = await resend.emails.send({
+  from: process.env.FROM_EMAIL,
+  to: process.env.TO_EMAIL,
+  subject: "New Contact Form Message - NMBK",
+  text: `
 Name: ${name}
 
 Email: ${email}
 
 Message:
 ${message}
-    `,
-  };
+  `,
+});
 
-  try {
-    const info = await transporter.sendMail(mailOptions);
 console.log("Email sent:", info);
 
     res.json({

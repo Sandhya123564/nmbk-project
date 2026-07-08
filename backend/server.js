441d52e53c4c98e2c5385a50d8721fb0bd9fbd4c
sandhya <sandhyadsan03@gmail.com>
2026-07-08 12:30:04 +0530

    Updated frontend API URL and fixed contact form

--- backend/server.js
+++ backend/server.js
@@ -3,35 +3,50 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { Resend } = require("resend");
+const nodemailer=require("nodemailer")
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 app.post("/contact", async (req, res) => {
   const { name, email, message } = req.body;
 
-  try {
-    await resend.emails.send({
-      from: "NMBK Contact <onboarding@resend.dev>",
-      to: process.env.EMAIL_USER,
-      subject: "New Contact Form Message - NMBK",
-      html: `
-        <h3>New Message</h3>
-        <p><b>Name:</b> ${name}</p>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Message:</b> ${message}</p>
-      `,
-    });
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: "New Contact Form Message - NMBK",
+    text: `
+Name: ${name}
 
-    res.json({ success: true, message: "Email sent successfully" });
+Email: ${email}
+
+Message:
+${message}
+    `,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+console.log("Email sent:", info);
+
+    res.json({
+      success: true,
+      message: "Email sent successfully",
+    });
 
   } catch (error) {
     console.log(error);
+
     res.status(500).json({
       success: false,
       message: "Email sending failed",
